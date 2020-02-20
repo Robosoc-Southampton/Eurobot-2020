625e7eb099f554ca306fb707215c25642776e338
flyingmat <paramattimatteo@gmail.com>
2020-02-20 22:44:27 +0000

    Say no to tabs

--- src/python/vision/main.py
+++ src/python/vision/main.py
@@ -6,18 +6,18 @@ from model import VisionModel
 
 
 class ImageProcessor(Thread):
-	def __init__(self, callback, camera: VisionCamera, vision: VisionModel):
-		Thread.__init__(self)
-		self.__callback = callback
-		self.camera = camera
+    def __init__(self, callback, camera: VisionCamera, vision: VisionModel):
+        Thread.__init__(self)
+        self.__callback = callback
+        self.camera = camera
 
-	def run(self):
-		# get object positions for robots, cups, etc. from the video stream
-		while objects := self.camera.videoStreamObjects():
-			# do stuff with the data (altering self.vision ?)
-			pass
+    def run(self):
+        # get object positions for robots, cups, etc. from the video stream
+        while objects := self.camera.videoStreamObjects():
+            # do stuff with the data (altering self.vision ?)
+            pass
 
 # start necessary background threads that process images
 def beginImageProcessing(callback, camera: VisionCamera, vision: VisionModel):
-	ip = ImageProcessor(callback, camera, vision)
-	ip.start()
+    ip = ImageProcessor(callback, camera, vision)
+    ip.start()
--- src/python/vision/model.py
+++ src/python/vision/model.py
@@ -5,24 +5,24 @@ from worldmodel.position import BoardPosition
 
 
 class CupColour(Enum):
-	GREEN = 0
-	RED = 1
+    GREEN = 0
+    RED = 1
 
 
 class CupModel:
-	def __init__(self, colour: CupColour, position: BoardPosition):
-		self.colour = colour
-		self.position = position
+    def __init__(self, colour: CupColour, position: BoardPosition):
+        self.colour = colour
+        self.position = position
 
 
 class RobotModel:
-	def __init__(self, id: int, position: BoardPosition):
-		self.id = id
-		self.position = position
+    def __init__(self, id: int, position: BoardPosition):
+        self.id = id
+        self.position = position
 
 
 class VisionModel:
-	def __init__(self, cups: List[CupModel] = [], robots: List[RobotModel] = [], side: CupColour):
-		self.cups = cups # list of cups detected
-		self.robots = robots # list of robots detected
-		self.side = side # side of the board we're playing on
+    def __init__(self, cups: List[CupModel] = [], robots: List[RobotModel] = [], side: CupColour):
+        self.cups = cups # list of cups detected
+        self.robots = robots # list of robots detected
+        self.side = side # side of the board we're playing on
